da324aa3633bb3f2462eb816833cb5c75b8c2fbc
NorthingMore <NothingMore@gmail.com>
2023-06-18 08:48:05 +0300

    Сделал дз2

---
+++ Homework2/Task13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 13: Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// Решение 1
+/*Console.WriteLine("Введите трехзначное число");
+string numbString = Console.ReadLine()!;
+int z=0;
+
+for(int i = 0; i < numbString.Length; i++){
+    z++;}
+
+if(z>=3){
+    int digetIndex = 2;
+    int digit = numbString[digetIndex]-'0';
+    int numb = Convert.ToInt32(digit);
+    Console.WriteLine(numb);}
+else
+     Console.WriteLine("третьей цифры нет");*/
+
+
+// Решение 2
+Console.WriteLine("Введите трехзначное число");
+int numb = Convert.ToInt32(Console.ReadLine()!);
+while (numb>999)
+{
+    numb /= 10;
+}
+Console.WriteLine( numb<100?// проверяем является ли число меньше 100
+"третьей цифры нет":numb%10);//если да, то пишем "третьей цифры нет", если нет, то находим 3 элемент
+
+
+
+    
---
+++ Homework2/Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Напишите номер дня недели");
+int numberDay = Convert.ToInt32(Console.ReadLine()!);
+
+if (numberDay >= 1&&numberDay<6){
+    Console.WriteLine("Эх, рабочий день");
+}
+else if(numberDay >= 6&&numberDay<8)
+    Console.WriteLine("Выходной, ура");
+
+else{
+    Console.WriteLine("Под таким номером нету дня недели");
+}
