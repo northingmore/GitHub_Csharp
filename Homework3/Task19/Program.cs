54ac7deda61023902c59ea4f3b0c2cbc18b38d82
NorthingMore <NothingMore@gmail.com>
2023-06-19 21:44:51 +0300

    Сделал дз3

---
+++ Homework3/Task19/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int numb = Convert.ToInt32(Console.ReadLine()!);
+int numbResult = 0;
+int numbTest=numb;
+while (numb > 0) {
+                numbResult *= 10;
+                numbResult += numb % 10;
+                numb /= 10;
+            }
+Console.WriteLine(numbResult);
+numb=numbTest;
+if (numb == numbResult)
+    Console.WriteLine("палиндром");
+else
+    Console.WriteLine("Нет, не палиндром");
+
---
+++ Homework3/Task21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату x1");
+double x1 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите координату y1");
+double y1 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите координату z1");
+double z1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату x2");
+double x2 = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите координату y2");
+double y2 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату z2");
+double z2 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine($"расстояние между ними в 3D пространстве {Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2)):f2}");
