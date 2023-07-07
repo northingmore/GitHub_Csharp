c6cbbf86fa798a3d817ca094b294916b5c673f6a
NorthingMore <NothingMore@gmail.com>
2023-07-07 03:23:34 +0300

    Дз 6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int NumberElementMass()
+{
+    Console.WriteLine("Введите число элементов массива");
+    int numberElementMass = int.Parse(Console.ReadLine()!);
+    return numberElementMass;
+}
+
+double[] CreatingArray()
+{
+    double[] array = new double[NumberElementMass()];
+    return array;
+}
+
+double[] InitializationArray(double[] masive)
+{
+    int i = 0;
+    double numbIElement=0;
+    while (i < masive.Length)
+    {
+        Console.WriteLine($"Введите число {i+1}-того эллемента массива: ");
+        numbIElement = double.Parse(Console.ReadLine()!);
+        masive[i] = numbIElement;
+        i++;
+    }
+    Console.WriteLine($"\nМассив -> [{string.Join(", ", masive)}]");
+    return masive;
+}
+
+
+double[] initializationArray = InitializationArray(CreatingArray());
+//double theNumberOfNumbersGreaterThan_0_EnteredByTheUser = TheNumberOfNumb(initializationArray);
+double count = 0;
+
+    for(int i = 0; i < initializationArray.Length;i++)
+    {
+        if(0<initializationArray[i])
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+double[] CreatingArray()
+{
+    double[] array = new double[4];
+    return array;
+}
+
+double[] InitializationArray(double[] masive)
+{
+    int i = 0;
+    double numbIElement=0;
+    while (i < masive.Length)
+    {
+        Console.WriteLine($"Введите число {i+1}-того эллемента массива: ");
+        numbIElement = double.Parse(Console.ReadLine()!);
+        masive[i] = numbIElement;
+        i++;
+    }
+    Console.WriteLine($"\nМассив -> [{string.Join(", ", masive)}]");
+    return masive;
+}
+
+double[] Validation(double[] masive)
+{
+    int i = 0;
+    while (i < masive.Length)
+    {
+        double b1 = 0;
+        double k1 = 0;
+        double b2 = 0;
+        double k2 = 0;
+        if(i==1)
+            b1 = masive[i];
+        else if(i==2)
+            k1 = masive[i];
+        else if(i==3)
+            b2 = masive[i];
+        else if(i==4)
+            k2=masive[i];
+        i++;
+    }
+    return masive;
+}
+
+void PointOfIntersection(double[] masive)
+{
+        double x = (masive[2] - masive[0]) / (masive[1] - masive[3]);
+        double y= masive[1] * x + masive[0];
+        Console.WriteLine("x = " + x + "y = " + y);
+}
+
+double[] initializationArray = Validation(InitializationArray(CreatingArray()));
+PointOfIntersection(initializationArray);
